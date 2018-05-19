901279c09371fdbfc3bdf9f9c40d59bb500fc559
railfanclub634 <railfanclub634hp@gmail.com>
2018-05-19 20:06:06 +0900

    1

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 /* jshint strict: false */
+window.onload = function(){
+	$('.box1').css({'background-image':'url(images/home/2018-96th-booklet.svg)','color':'#111'});
+	$('.box2').css({'background-image':'url(images/home/2018-harutyouki.png)','color':'#FFF'});
+	$('.box3').css({'background-image':'url(images/home/2018-kinensai-logo.png)','color':'#FFF'});
+};
