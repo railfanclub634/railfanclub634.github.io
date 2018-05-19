1f1f68f7e178e99bad118c228b1084d0eef08f2e
railfanclub634 <railfanclub634hp@gmail.com>
2018-05-20 01:05:45 +0900

    1

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ window.onload = function(){
 	
 	//css書き換え
 	$('.box1').css({'background-image':'url(images/home/site-introduction.png)','color':'#FFF','width':'100%'});
-	$('.box2').css({'background-image':'url(images/home/2018-harutyouki.png)','color':'#FFF'});
-	$('.box3').css({'background-image':'url(images/home/2018-kinensai-logo.png)','color':'#FFF'});
+	$('.box2').css({'background-image':'url(images/home/2018-96th-booklet.svg)','color':'#FFF'});
+	$('.box3').css({'background-image':'url(images/home/2018-harutyouki.png)','color':'#FFF'});
+	$('.box4').css({'background-image':'url(images/home/2018-kinensai-logo.png)','color':'#FFF'});
 };
