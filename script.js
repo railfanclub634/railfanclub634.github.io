cb9ccdffc1fb064856b00a614b83df84c3d50612
railfanclub634 <38607428+railfanclub634@users.noreply.github.com>
2018-05-23 20:47:31 +0900

    1

--- script.js
+++ script.js
@@ -7,10 +7,10 @@
 	
 	
 	//css書き換え
-	$('.box1').css({'background-image':'url(images/home/site-introduction.png)','color':'#FFF','width':'100%'});
-	$('.box2').css({'background-image':'url(images/home/2018-96th-booklet.svg)','color':'#111'});
-	$('.box3').css({'background-image':'url(images/home/2018-harutyouki.png)','color':'#FFF'});
-	$('.box4').css({'background-image':'url(images/home/2018-kinensai-logo.png)','color':'#FFF'});
+	$('.box1').css({'background-image':'url(images/site-introduction.png)','color':'#FFF','width':'100%'});
+	$('.box2').css({'background-image':'url(images/2018-96th-booklet.svg)','color':'#111'});
+	$('.box3').css({'background-image':'url(images/2018-harutyouki.png)','color':'#FFF'});
+	$('.box4').css({'background-image':'url(images/2018-kinensai-logo.png)','color':'#FFF'});
 	
 	//boxにリンク
 	$('.box1').click(function() {
