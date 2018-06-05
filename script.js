f1e795a4b2c06c47501612e66048ae6a80bf0772
railfanclub634 <railfanclub634hp@gmail.com>
2018-06-05 20:41:07 +0900

    1

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
 /*JShint false*/
 $(function(){
-	//html書き換え
-	$('.box1').html('<h4>2018/05/19</h4><h1>HP開設</h1>');
-	$('.box2').html('<h4>2018/05/19</h4><h1>小冊子配布</h1>');
-	$('.box3').html('<h4>2018/05/19</h4><h1>春長期旅行</h1>');
-	$('.box4').html('<h4>2018/05/19</h4><h1>第96回記念祭</h1>');
-	
-	
-	//css書き換え
-	$('.box1').css({'background-image':'url(images/site-introduction.png)','color':'#FFF','width':'100%'});
-	$('.box2').css({'background-image':'url(images/2018-96th-booklet.svg)','color':'#111'});
-	$('.box3').css({'background-image':'url(images/2018-harutyouki.png)','color':'#FFF'});
-	$('.box4').css({'background-image':'url(images/2018-kinensai-logo.png)','color':'#FFF'});
-	
+
 	//boxにリンク
 	$('.box1').click(function() {
-		window.location.href="./sites/special/site-opening/index.html"
+		window.location.href = "./sites/special/site-opening/index.html"
  	}); 
 	 
 	//fixed-buttonクリックイベント
