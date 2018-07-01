95525ea6792e7dd099a275d7b2db48addbe3844a
railfanclub634 <railfanclub634hp@gmail.com>
2018-07-01 09:59:31 +0900

    1

--- script.js
+++ script.js
@@ -5,14 +5,5 @@ $(function(){
 	$('.box1').click(function() {
 		window.location.href = "./sites/special/site-opening/index.html"
  	}); 
-	 
-	//fixed-buttonクリックイベント
-	$('.fixed-button-colider').click(function() {
-		$('.overlay').toggleClass('is-open');
- 	});
-/*
-	$('.overlay').click(function() {
-		$('.overlay').removeClass('is-open');
- 	});
-*/
+	
 });
