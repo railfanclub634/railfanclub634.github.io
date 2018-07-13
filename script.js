960122c5e0b658002f210f9cad946905244052e9
railfanclub634 <railfanclub634hp@gmail.com>
2018-07-13 23:12:25 +0900

    1

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-/*JShint false*/
 $(function(){
 
 	//boxにリンク
 	$('.box1').click(function() {
-		window.location.href = "./sites/special/site-opening/index.html"
+		window.location.href = "/sites/special/site-opening/index.html"
+ 	}); 
+
+	$('.box2').click(function() {
+		window.location.href = "/sites/special/site-opening/index.html"
+ 	}); 
+
+	$('.box3').click(function() {
+		window.location.href = "/sites/special/site-opening/index.html"
+ 	}); 
+
+	$('.box4').click(function() {
+		window.location.href = "/sites/special/site-opening/index.html"
  	}); 
 	
 });
