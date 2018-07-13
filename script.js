8d7e944b015e0026112dbe2a047eb0bbcc82d31b
railfanclub634 <railfanclub634hp@gmail.com>
2018-07-13 23:27:06 +0900

    1

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ $(function(){
  	}); 
 
 	$('.box2').click(function() {
-		window.location.href = "/sites/special/site-opening/index.html"
+		window.location.href = "/sites/haihu/96th-booklet/index.html"
  	}); 
 
 	$('.box3').click(function() {
-		window.location.href = "/sites/special/site-opening/index.html"
+		window.location.href = "/sites/katudou/389/index.html"
  	}); 
 
 	$('.box4').click(function() {
-		window.location.href = "/sites/special/site-opening/index.html"
+		window.location.href = "#"
  	}); 
 	
 });
