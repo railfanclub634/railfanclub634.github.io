3b76d459842d1c397051e793367b03265af06e09
okaari <oka1202ari@gmail.com>
2018-08-15 01:36:47 +0900

    1

--- sites/test/getJSON.js
+++ sites/test/getJSON.js
@@ -1,11 +1,10 @@
 $(function() {
 //URLパラメーター読み込み
   var jsonname = "test";
-//json読み込み
-  $.getJSON("test.json", {name: "blogdata"}, function(data){
-	   var dataArray = data.blogdata;
-     $.each(dataArray, function(i){
+//  $.getJSON("test.json", {name: "blogdata"}, function(data){
+//	   var dataArray = data.blogdata;
+//     $.each(dataArray, function(i){
 		     $("main").append("<div class="title"><h1>" + dataArray[i].title +"</h1></div>");
 	       });
-    });
-});
+//    });
+//});
