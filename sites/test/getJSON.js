f5d1d009ffe5844991e11bbb3c31b5e78f91e519
okaari <oka1202ari@gmail.com>
2018-08-14 22:01:44 +0900

    1

--- sites/test/getJSON.js
+++ sites/test/getJSON.js
@@ -2,7 +2,7 @@ $(function() {
 //URLパラメーター読み込み
   var jsonname = "test";
 //json読み込み
-  $.getJSON("json/data.json", {name: "blogdata"}, function(data){
+  $.getJSON("test.json", {name: "blogdata"}, function(data){
 	   var dataArray = data.blogdata;
      $.each(dataArray, function(i){
 		     $("main").append("<div class="title">" + dataArray[i].title +"</div>");
