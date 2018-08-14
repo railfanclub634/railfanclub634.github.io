517353a0985326bad972d8d470a4c4348082f94a
okaari <oka1202ari@gmail.com>
2018-08-15 01:38:23 +0900

    1

--- sites/test/getJSON.js
+++ sites/test/getJSON.js
@@ -1,10 +1,11 @@
 $(function() {
 //URLパラメーター読み込み
-  var jsonname = "test";
+//  var jsonname = "test";
 //  $.getJSON("test.json", {name: "blogdata"}, function(data){
 //	   var dataArray = data.blogdata;
 //     $.each(dataArray, function(i){
-		     $("main").append("<div class="title"><h1>" + dataArray[i].title +"</h1></div>");
+//		     $("main").append("<div class="title"><h1>" + dataArray[i].title +"</h1></div>");
+         $("main").append("<div class="title"><h1>" + タイトル +"</h1></div>");
 	       });
 //    });
 //});
