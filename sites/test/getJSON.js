25dc1cf57965a6773b6f4f84140ad59a9f30903b
railfanclub634 <railfanclub634hp@gmail.com>
2018-09-01 10:10:18 +0900

    add getjson

--- sites/test/getJSON.js
+++ sites/test/getJSON.js
@@ -1,12 +1,10 @@
-$(function() {
+window.addEventListener('DOMContentLoaded', function() {
 //URLパラメーター読み込み
   var jsonname = "test";
   $.getJSON("test.json", {name: "blogdata"}, function(data){
 	   var dataArray = data.blogdata;
      $.each(dataArray, function(i){
-		     $("main").append("<div class="title"><h1>" + dataArray[i].title +"</h1></div>");
-         $("main").append("<div class="title"><h1>" + タイトル +"</h1></div>"
-       );
+		     $("main").append('<div class="title"><h1>' + dataArray[i].title +'</h1></div></div><div class="toukousya"><p>投稿者:' + dataArray[i].name + ' 投稿日:' + dataArray[i].day + '</p></div><div class="text"><p>' + dataArray[i].text + '</p></div>');
 	       });
     });
 });
