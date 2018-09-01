ebdf2f22ad247b8709e03a0a1be1f7600a5df9cd
railfanclub634 <railfanclub634hp@gmail.com>
2018-09-01 12:20:20 +0900

    1

--- sites/getJSON.js
+++ sites/getJSON.js
@@ -3,10 +3,10 @@ window.addEventListener('DOMContentLoaded', function() {
   var jsonname = document.location.search.substring(1) + "/data.json";
   var headerimg= document.location.search.substring(1) + "/header.png";
   $.getJSON(jsonname, {name: "blogdata"}, function(data){
-	   var dataArray = data.blogdata;
-     $.each(dataArray, function(i){
+	var dataArray = data.blogdata;
+    $.each(dataArray, function(i){
          $(".headerimg").append('<img class="second-header" src="' + headerimg + '" alt="second-header">');
-		     $("main").append('<div class="title"><h1>' + dataArray[i].title +'</h1></div></div><div class="toukousya"><p>投稿者:' + dataArray[i].name + ' 投稿日:' + dataArray[i].day + '</p></div><div class="text"><p>' + dataArray[i].text + '</p></div>');
+		 $("main").append('<div class="title"><h1>' + dataArray[i].title +'</h1></div></div><div class="toukousya"><p>投稿者:' + dataArray[i].name + ' 投稿日:' + dataArray[i].day + '</p></div><div class="text"><p>' + dataArray[i].text + '</p></div>');
 	       });
     });
 });
---
+++ sites/list/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', function() {
+    var jsonname = document.location.search.substring(1) + "-list.json"
+    $.getJSON(jsonname, {name: "listdata"}, function(data){
+        var dataArray = data.listdata;
+      $.each(dataArray, function(i){
+          $(".list").append('<a href=/sites?' + jsonname + '/' + dataArray[i].url + '><li>' + dataArray[i].title + '</li></a>');
+        });
+    });
+});
