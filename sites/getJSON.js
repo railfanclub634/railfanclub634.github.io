9832087d3919a4c89f9f77076e95ffd18ff1cacc
railfanclub634 <railfanclub634hp@gmail.com>
2018-09-05 05:04:31 +0900

    1

--- sites/getJSON.js
+++ sites/getJSON.js
@@ -1,12 +1,16 @@
 window.addEventListener('DOMContentLoaded', function() {
 //URLパラメーター読み込み
   var jsonname = document.location.search.substring(1) + "/data.json";
-  var headerimg= document.location.search.substring(1) + "/header.png";
+  var headerimg = document.location.search.substring(1) + "/header.png";
   $.getJSON(jsonname, {name: "blogdata"}, function(data){
 	var dataArray = data.blogdata;
     $.each(dataArray, function(i){
-         $(".headerimg").append('<img class="second-header" src="' + headerimg + '" alt="second-header">');
-		 $("main").append('<div class="title"><h1>' + dataArray[i].title +'</h1></div></div><div class="toukousya"><p>投稿者:' + dataArray[i].name + ' 投稿日:' + dataArray[i].day + '</p></div><div class="text"><p>' + dataArray[i].text + '</p></div>');
+		     $("main").append('<div class="title"><h1>' + dataArray[i].title +'</h1></div></div><div class="toukousya"><p>投稿者:' + dataArray[i].name + ' 投稿日:' + dataArray[i].day + '</p></div><div class="text"><p>' + dataArray[i].text + '</p></div>');
 	       });
     });
+    $(".headerimg").append('<img class="second-header" src=' + headerimg + ' alt="second-header">');
 });
+window.onload = function(){
+  var imgname = "<img src=" + document.location.search.substring(1) +"/1.png>";
+  document.getElementById("1").innerHTML = imgname;
+};
