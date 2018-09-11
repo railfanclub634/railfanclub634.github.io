5e413878f55eb56e50a6f53227769961181c4ce5
okaari <oka1202ari@gmail.com>
2018-09-11 23:36:28 +0900

    delete all

--- common/scripts/Animation.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-  var scroll = $(window).scrollTop();
-	$(".second-header").css({
-		width: (100 + scroll/5)  + "%",
-		top: -(scroll/10)  + "%",
-		"-webkit-filter": "blur(" + (scroll/200) + "px)",
-		filter: "blur(" + (scroll/200) + "px)"
-	});
-});
--- list/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-    var jsonname = "json/" + document.location.search.substring(1) + "-list.json"
-    $.getJSON(jsonname, {name: "listdata"}, function(data){
-        var dataArray = data.listdata;
-      $.each(dataArray, function(i){
-          $(".list").append('<a href=/sites?' + document.location.search.substring(1) + '/' + dataArray[i].url + '><li>' + dataArray[i].title + '</li></a>');
-        });
-    });
-});
